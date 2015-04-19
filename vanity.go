5adce6f5f2520e20efb9c4d1a697d9cbf71b0640
Joshua Hawxwell <m@hawx.me>
2015-04-19 19:53:41 +0100

    Initial commit

---
+++ vanity.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/hawx/serve"
+)
+
+var (
+	port   = flag.String("port", "8080", "")
+	socket = flag.String("socket", "", "")
+)
+
+type configRow struct {
+	Prefix, Vcs, Url string
+}
+type Config []configRow
+
+func (c Config) Len() int           { return len(c) }
+func (c Config) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c Config) Less(i, j int) bool { return c[i].Prefix > c[j].Prefix }
+
+func Server(conf Config) http.Handler {
+	sort.Sort(conf)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Method Not Allowed", 405)
+			return
+		}
+
+		host, _, err := net.SplitHostPort(r.Host)
+		if err != nil {
+			host = r.Host
+		}
+
+		log.Println(r.URL)
+
+		if r.FormValue("go-get") != "1" {
+			http.Redirect(w, r, "http://godoc.org/"+host+r.URL.Path, http.StatusFound)
+			return
+		}
+
+		for _, row := range conf {
+			if strings.HasPrefix(r.URL.Path, row.Prefix) {
+				fmt.Fprintf(w, `<meta name="go-import" content="%s%s %s %s">`, host, row.Prefix, row.Vcs, row.Url)
+				return
+			}
+		}
+	})
+}
+
+func DecodeConfig(r io.Reader) (Config, error) {
+	config := Config{}
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+
+		switch len(fields) {
+		case 0:
+			continue
+		case 3:
+			config = append(config, configRow{fields[0], fields[1], fields[2]})
+		default:
+			return nil, fmt.Errorf("config malformed: %s", scanner.Text())
+		}
+	}
+
+	return config, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Fprint(os.Stderr, "Usage: vanity [--port PORT | --socket SOCK] CONFIG\n")
+		os.Exit(1)
+	}
+
+	configFile, err := os.Open(flag.Args()[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config, err := DecodeConfig(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	serve.Serve(*port, *socket, Server(config))
+}
---
+++ vanity_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestVanity(t *testing.T) {
+	server := httptest.NewServer(Server(Config{
+		{"/example", "git", "git://example.org/example"},
+		{"/", "git", "https://code.org/r/p/exproj"},
+		{"/example/pkg", "git", "git://example.org/expkg"},
+	}))
+	defer server.Close()
+
+	table := []struct {
+		req, resp string
+	}{
+		{"/example?go-get=1", "127.0.0.1/example git git://example.org/example"},
+		{"/pkg/foo?go-get=1", "127.0.0.1/ git https://code.org/r/p/exproj"},
+		{"/example/pkg/foo?go-get=1", "127.0.0.1/example/pkg git git://example.org/expkg"},
+	}
+
+	for _, tc := range table {
+		resp, err := http.Get(server.URL + tc.req)
+		if err != nil {
+			t.Fatalf("Get: %v", err)
+		}
+
+		body, _ := ioutil.ReadAll(resp.Body)
+		if string(body) != `<meta name="go-import" content="`+tc.resp+`">` {
+			t.Fatalf("body incorrect, got: %s", body)
+		}
+	}
+}
+
+func TestVanityNotGoTool(t *testing.T) {
+	server := httptest.NewServer(Server(Config{
+		{"/example", "git", "git://example.org/example"},
+	}))
+	defer server.Close()
+
+	resp, err := http.Get(server.URL + "/example")
+	if err != nil {
+		t.Fatalf("Get: %v", err)
+	}
+
+	if resp.Request.URL.String() != "http://godoc.org/127.0.0.1/example" {
+		t.Fatalf("Expected redirect, Got: %s", resp.Request.URL)
+	}
+}
+
+func TestReadConfig(t *testing.T) {
+	r := strings.NewReader(`/example git git://example.org/example
+/what  hg    https://example.com/what`)
+
+	conf, err := DecodeConfig(r)
+	if err != nil {
+		t.Fatalf("DecodeConfig: %v", err)
+	}
+
+	expected := Config{
+		{"/example", "git", "git://example.org/example"},
+		{"/what", "hg", "https://example.com/what"},
+	}
+
+	if len(conf) != 2 || conf[0] != expected[0] || conf[1] != expected[1] {
+		t.Fatalf("Expected: %v, Got: %v", expected, conf)
+	}
+}
