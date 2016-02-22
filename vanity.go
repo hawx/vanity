2ca1407aa6a6ed29cf1848e89014c2e632eebf03
Joshua Hawxwell <m@hawx.me>
2016-02-22 17:48:09 +0000

    Use a map to store config

--- vanity.go
+++ vanity.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sort"
 	"strings"
 
 	"hawx.me/code/serve"
@@ -19,38 +18,38 @@ var (
 	socket = flag.String("socket", "", "")
 )
 
-type configRow struct {
-	Prefix, Vcs, Url string
+type packageConfig struct {
+	Prefix, VCS, URL string
 }
-type Config []configRow
-
-func (c Config) Len() int           { return len(c) }
-func (c Config) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c Config) Less(i, j int) bool { return c[i].Prefix > c[j].Prefix }
+type Config map[string]packageConfig
 
 func Server(host string, conf Config) http.Handler {
-	sort.Sort(conf)
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
-			http.Error(w, "Method Not Allowed", 405)
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
 		log.Println(r.URL)
+		
+		if r.URL.Path[len(r.URL.Path)-1] == '/' {
+			http.Redirect(w, r, r.URL.Path[:len(r.URL.Path)-1], http.StatusMovedPermanently)
+			return
+		}
+
+		row, ok := conf[r.URL.Path]
+		if !ok {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
 
 		if r.FormValue("go-get") != "1" {
 			http.Redirect(w, r, "http://godoc.org/"+host+r.URL.Path, http.StatusFound)
 			return
 		}
 
-		for _, row := range conf {
-			if strings.HasPrefix(r.URL.Path, row.Prefix) {
-				w.Header().Add("Content-Type", "text/html")
-				fmt.Fprintf(w, `<meta name="go-import" content="%s%s %s %s">`, host, row.Prefix, row.Vcs, row.Url)
-				return
-			}
-		}
+		w.Header().Add("Content-Type", "text/html")
+		fmt.Fprintf(w, `<meta name="go-import" content="%s%s %s %s">`, host, row.Prefix, row.VCS, row.URL)
 	})
 }
 
@@ -65,7 +64,7 @@ func DecodeConfig(r io.Reader) (Config, error) {
 		case 0:
 			continue
 		case 3:
-			config = append(config, configRow{fields[0], fields[1], fields[2]})
+			config[fields[0]] = packageConfig{fields[0], fields[1], fields[2]}
 		default:
 			return nil, fmt.Errorf("config malformed: %s", scanner.Text())
 		}
@@ -74,12 +73,14 @@ func DecodeConfig(r io.Reader) (Config, error) {
 	return config, nil
 }
 
+const usage = "Usage: vanity [--port PORT | --socket SOCK] HOST CONFIG\n"
+
 func main() {
 	flag.Parse()
 	argv := flag.Args()
 
 	if len(argv) != 2 {
-		fmt.Fprint(os.Stderr, "Usage: vanity [--port PORT | --socket SOCK] HOST CONFIG\n")
+		fmt.Fprint(os.Stderr, usage)
 		os.Exit(1)
 	}
 
